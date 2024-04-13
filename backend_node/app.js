e7221fb6d3cd47490a93da5bb562b5a8a12beac6
GoodManWEN <44322449+GoodManWEN@users.noreply.github.com>
2024-04-13 08:35:43 +0800

    Update app.js

--- backend_node/app.js
+++ backend_node/app.js
@@ -3,16 +3,16 @@ const app = express();
 const router = require('./router');
 const cookieParser = require('cookie-parser');
 
-// routing static files
-app.use(express.static('assets'));
+
 
 // use express.json() middleware to parse JSON bodies
 app.use(express.json());
-
 // use router
 app.use(router);
 // use cookieParser middleware
 app.use(cookieParser());
+// routing static files
+app.use(express.static('assets'));
 
 app.listen(8080, () => {
     console.log('Server is running on http://localhost:8080');
